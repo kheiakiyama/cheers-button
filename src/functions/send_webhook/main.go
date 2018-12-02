ab01e54f82b3de1b0ce328c0067e06c421a1726c
kheiakiyama <kheiakiyama@gmail.com>
2018-12-02 21:39:39 +0900

    send http post

--- src/functions/send_webhook/main.go
+++ src/functions/send_webhook/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -22,6 +26,29 @@ type MyEvent struct {
 // HandleRequest puts lastmodified to s3
 func HandleRequest(ctx context.Context, event MyEvent) (string, error) {
 	log.Print(event)
+	var webhookURL = os.Getenv("WEBHOOK_URL")
+	params, err := json.Marshal(event)
+	if err != nil {
+		return "", err
+	}
+	req, err := http.NewRequest(
+		"POST",
+		webhookURL,
+		bytes.NewBuffer(params),
+	)
+	if err != nil {
+		return "", err
+	}
+
+	// Content-Type 設定
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
 	return "normal end", nil
 }
 
