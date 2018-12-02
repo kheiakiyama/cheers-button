231deb3603fd1b0846c0860b1d9a0f0baca4ab89
kheiakiyama <kheiakiyama@gmail.com>
2018-12-02 21:39:45 +0900

    fix name

--- functions.tf
+++ functions.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "iot_button_metrics" {
-  name = "iot_button_metrics"
+resource "aws_iam_role" "iot_button_send_metric" {
+  name = "iot_button_send_metric"
 
   assume_role_policy = <<EOF
 {
@@ -18,28 +18,14 @@ resource "aws_iam_role" "iot_button_metrics" {
 EOF
 }
 
-resource "aws_iam_role_policy" "iot_button_metrics" {
-  name = "iot_button_metrics"
-  role = "${aws_iam_role.iot_button_metrics.id}"
+resource "aws_iam_role_policy" "iot_button_send_metric" {
+  name = "iot_button_send_metric"
+  role = "${aws_iam_role.iot_button_send_metric.id}"
 
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
-    {
-       "Effect": "Allow",
-       "Action": ["s3:ListBucket"],
-       "Resource": ["${aws_s3_bucket.metrics.arn}"]
-     },
-     {
-       "Effect": "Allow",
-       "Action": [
-         "s3:PutObject",
-         "s3:GetObject",
-         "s3:DeleteObject"
-       ],
-       "Resource": ["${aws_s3_bucket.metrics.arn}/*"]
-     },
      {
        "Effect": "Allow",
        "Action": [
@@ -55,7 +41,7 @@ EOF
 resource "aws_lambda_function" "send_webhook" {
   filename         = "bin/send_webhook.zip"
   function_name    = "send_webhook"
-  role             = "${aws_iam_role.iot_button_metrics.arn}"
+  role             = "${aws_iam_role.iot_button_send_metric.arn}"
   handler          = "bin/send_webhook"
   source_code_hash = "${base64sha256(file("bin/send_webhook.zip"))}"
   runtime          = "go1.x"
